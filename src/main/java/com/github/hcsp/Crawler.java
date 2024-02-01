f65da769d859a7bdadf4f524b6eb568d83dfcb88
Yang.he <yang.he@homepartners.tech>
2024-02-01 14:16:01 +0800

    refactor modules

---
+++ src/main/java/com/github/hcsp/Crawler.java
@@ -0,0 +1,106 @@
+package com.github.hcsp;
+
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
+import org.apache.http.HttpEntity;
+import org.apache.http.client.config.CookieSpecs;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.stream.Collectors;
+
+public class Crawler {
+    private CrawlerDao dao = new JdbcCrawlerDao();
+    public void run() throws SQLException, IOException {
+        String link;
+        while ((link = dao.getNextLinkThenDelete()) != null) {
+            if (dao.inLinkProcessed(link)) {
+                continue;
+            }
+            if (isInterestingLink(link)) {
+                Document doc = httpGetAndParseHtml(link);
+                parseUrlsFromPageAndStoreIntoDatabase(doc);
+                storeIntoDatabase(doc, link);
+                dao.updateDatabase(link, "insert into LINKS_ALREADY_PROCESSED (link) values (?)");
+            }
+        }
+    }
+
+    @SuppressFBWarnings("DMI_CONSTANT_DB_PASSWORD")
+    public static void main(String[] args) throws IOException, SQLException {
+        new Crawler().run();
+    }
+
+    private void parseUrlsFromPageAndStoreIntoDatabase(Document doc) throws SQLException {
+        for (Element aTag : doc.select("a")) {
+            String href = aTag.attr("href");
+            processLink(href);
+        }
+    }
+
+    private void processLink(String link) throws SQLException {
+        if (link.startsWith("//")) {
+            link = "https:" + link;
+        }
+        if (!link.toLowerCase().startsWith("javascript")) {
+            dao.updateDatabase(link, "INSERT INTO LINKS_TO_BE_PROCESSED (link) VALUES (?)");
+        }
+    }
+
+    private void storeIntoDatabase(Document doc, String link) throws SQLException {
+        ArrayList<Element> articleTags = doc.select("article");
+        if (!articleTags.isEmpty()) {
+            for (Element articleTag : articleTags) {
+                String title = articleTag.child(0).text();
+                String content = articleTag.select("p").stream().map(Element::text).collect(Collectors.joining("\n"));
+                System.out.println(title);
+                dao.insertNewsIntoDatabase(link, title, content);
+            }
+        }
+    }
+
+    private static Document httpGetAndParseHtml(String link) throws IOException {
+        CloseableHttpClient httpClient = HttpClients.createDefault();
+        HttpGet httpGet = new HttpGet(link);
+        if(isKLink(link)) {
+            RequestConfig defaultConfig = RequestConfig.custom().setCookieSpec(CookieSpecs.IGNORE_COOKIES).build();
+            httpGet.setConfig(defaultConfig);
+        }
+        httpGet.addHeader("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36");
+        try(CloseableHttpResponse response1 = httpClient.execute(httpGet)) {
+            HttpEntity entity1 = response1.getEntity();
+            String html = EntityUtils.toString(entity1);
+            return Jsoup.parse(html);
+        }
+    }
+
+    private static boolean isInterestingLink(String link) {
+        return  isAllLink(link) && isNotValidLink(link);
+    }
+    private static boolean isNotValidLink(String link) {
+        return !link.contains("passport.sina.cn")
+                && !link.contains("share.sina.cn")
+                && !link.contains("mail.sina.cn")
+                && !link.contains("games.sina.cn")
+                && !link.contains("photo.sina.cn")
+                && !link.contains("health.sina.cn")
+                && !link.contains("jiaju.sina.cn")
+                && !link.contains("zhibo.sina.cn");
+    }
+
+    private static boolean isKLink(String link) {
+        return link.contains("k.sina.cn");
+    }
+    private static boolean isAllLink(String link) {
+        return link.contains("sina.cn");
+    }
+}
